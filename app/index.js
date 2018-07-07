9026fbbfdb780b5b5eeb7bf9968fade34f79644c
Michael Pompili <michael@churroboros.com>
2018-07-07 14:41:23 -0700

    create Popular navbar

--- app/index.js
+++ app/index.js
@@ -1,19 +1,12 @@
 var React = require('react'); 
 var ReactDOM = require('react-dom');
+var App = require('./components/App'); 
 require('./index.css');
 
 //state
 //lifecycle event - way to know when a certain event happens 
 //UI of the component (only one required);
 
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Got it working</h1> 
-            </div>
-        ); 
-    }
-}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
