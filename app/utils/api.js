147483f84ea25a45cad0907362b1ae355905491c
Michael Pompili <michael@churroboros.com>
2018-07-10 14:44:47 -0700

    being refactoring

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,30 +1,32 @@
-var axios = require('axios'); 
-var id = "YOUR_CLIENT_ID";
-var sec = "YOUR_SECRET_ID";
-var params = "?client_id=" + id + "&client_secret=" + sec; 
+const axios = require('axios'); 
+const id = "YOUR_CLIENT_ID";
+const sec = "YOUR_SECRET_ID";
+const params = `?client_id=${id}&client_secret=${sec}`; 
+// const params = "?client_id=" + id + "&client_secret=" + sec; 
 
 function getProfile (username) {
-    return axios.get('https://api.github.com/users/' + username + params)
-        .then(function(user) {
-            return user.data;
-        });
+      return axios.get(`https://api.github.com/users/${username}${params}`)
+          .then(user => user.data); 
+    // return axios.get('https://api.github.com/users/' + username + params)
+    //     .then(function(user) {
+    //         return user.data;
+    //     });
 }
 
 function getRepos(username) {
-    return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100');
+    // return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100');
+    return axios.get(`https://api.github.com/users/${username}/repos${params}per_page=100`); 
 }
 
 function getStarCount(repos) {
-    return repos.data.reduce((count, repo) => {
-        return count + repo.stargazers_count; 
-    }, 0);
+    return repos.data.reduce((count, { stargazers_count }) => count + stargazers_count, 0); 
+    // return repos.data.reduce((count, repo) => {
+    //     return count + repo.stargazers_count; 
+    // }, 0);
 }
 
-function calculateScore(profile, repos) {
-    var followers = profile.followers; 
-    var totalStars = getStarCount(repos);
-
-    return (followers * 3) + totalStars; 
+function calculateScore({followers}, repos) {
+    return (followers * 3) + getStarCount(repos); 
 }
 
 function handleError(error) {
@@ -47,9 +49,10 @@ function getUserData(player) {
 }
 
 function sortPlayers(players) {
-    return players.sort(function(a, b) {
-        return b.score - a.score;
-    });
+    return players.sort((a, b) => b.score - a.score); 
+    // return players.sort(function(a, b) {
+    //     return b.score - a.score;
+    // });
 }
 
 module.exports = {
@@ -61,11 +64,13 @@ module.exports = {
     },
 
     fetchPopularRepos: function(language) {
-        var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories');
-
+        var encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`); 
+        // var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories');
         return axios.get(encodedURI)
-            .then(function (response) {
-                return response.data.items;
-            });
+            .then(response => response.data.items); 
+        // return axios.get(encodedURI)
+        //     .then(function (response) {
+        //         return response.data.items;
+        //     });
     }
 };
